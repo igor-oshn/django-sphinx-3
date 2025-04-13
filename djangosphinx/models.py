4fb4f02834fbed67b84cc1eafa5da5cb6743efc4
Igor Oshnurov <oshnurov_i@garpix.com>
2025-04-14 01:14:06 +0300

    Fix bug

--- djangosphinx/models.py
+++ djangosphinx/models.py
@@ -428,7 +428,7 @@ class SphinxQuerySet(object):
 
         params = []
         
-        if isinstance(self._select, str):
+        if self._select and isinstance(self._select, str):
             client.SetSelect(self._select)
 
         if self._sort:
